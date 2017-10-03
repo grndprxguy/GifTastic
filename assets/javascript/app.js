091e7e6e59ed234ee7cd4277c81da55cdaddef5b
Matthew Nagy <mattn@Matts-MBP.triangle-mls.biz>
2017-10-03 15:54:50 -0400

    buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,14 @@
-$("#first").text("more buttons");
-console.log("test");
+var animalArray = ["Alligator", "Chameleon","Gecko", "Gila Monster", "Monitor Lizard", "Skink", "Iguana", "Bearded Dragon", "Viper", "Python", "Boa", "Rattlesnake", "Adder"]
+var selection = 0;
+
+$(document).ready(function() {
+	for(i = 0; i < animalArray.length; i++) {
+		var choice = animalArray[i];
+		var button = $('<button>');
+		button.text(choice);
+		button.attr('data-id', choice);
+		$('#animalButtons').append(button);
+	}
+});
+
+
