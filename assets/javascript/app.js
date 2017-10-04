202414f58dcfe28635f75f591e044721637d6e0f
Matthew Nagy <mattn@Matts-MBP.triangle-mls.biz>
2017-10-04 15:36:38 -0400

    buttons and images working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 var animalArray = ["Alligator", "Chameleon","Gecko", "Gila Monster", "Monitor Lizard", "Skink", "Iguana", "Bearded Dragon", "Viper", "Python", "Boa", "Rattlesnake", "Adder"]
 var selection = 0;
-
+$(document).ready(function() {
 
 function showButtons() {
+	$("#animalButtons").empty();
 	for(i = 0; i < animalArray.length; i++) {
 		var choice = animalArray[i];
 		var button = $('<button>');
@@ -13,34 +14,38 @@ function showButtons() {
 };
 
 // click button to call gify api and show images
-$(document).on("click", "button", function() {
-	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + selection;
-	selection = $(this).attr("data-id");
-	console.log(selection);
-	 $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .done(function(response) {
-          var results = response.data;
-          console.log(results);
-          for (var i = 0; i < results.length; i++) {
-          	var animalsDiv = $("<div>");
-          	var p = $("<p>").text("Rating: " + results[i].rating);
-          	var image = $("<img>");
-          	image.attr("src", results[i].images.fixed_height.url);
-          	animalsDiv.append(p);
-          	animalsDiv.append(image);
-          	console.log(animalsDiv);
-          	$("#animals").prepend(animalsDiv);
-          }
-      });
-
-
-});
-
-$(document).ready(function() {
-	showButtons();
+	$("#animalButtons").on("click","button",function() {
+		$("#animals").empty();
+		selection = $(this).attr("data-id");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + selection;
+		console.log(selection);
+		 $.ajax({
+	          url: queryURL,
+	          method: "GET"
+	        })
+	        .done(function(response) {
+	          var results = response.data;
+	          console.log(results);
+	          for (var i = 0; i < results.length; i++) {
+	          	var animalsDiv = $("<div class='animalsDiv'>");
+	          	var p = $("<p>").text("Rating: " + results[i].rating);
+	          	var image = $("<img>");
+	          	image.attr("src", results[i].images.fixed_height.url);
+	          	animalsDiv.append(p);
+	          	animalsDiv.append(image);
+	          	console.log(animalsDiv);
+	          	$("#animals").append(animalsDiv);
+	          }
+	      });
 	});
+
+	$("#add-animal").on("click",function(event){
+		event.preventDefault();
+		var addAnimal = $("#animal-input").val().trim();
+		animalArray.push(addAnimal);
+		showButtons();
+		});
+showButtons();
+});
 
 
