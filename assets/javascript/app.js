03162c89efdc322640a4e6ba9784fea69e702ed4
Matthew Nagy <grndprxguy@gmail.com>
2017-10-04 14:25:03 -0400

    giphy working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 var animalArray = ["Alligator", "Chameleon","Gecko", "Gila Monster", "Monitor Lizard", "Skink", "Iguana", "Bearded Dragon", "Viper", "Python", "Boa", "Rattlesnake", "Adder"]
 var selection = 0;
 
-$(document).ready(function() {
+
+function showButtons() {
 	for(i = 0; i < animalArray.length; i++) {
 		var choice = animalArray[i];
 		var button = $('<button>');
@@ -9,6 +10,37 @@ $(document).ready(function() {
 		button.attr('data-id', choice);
 		$('#animalButtons').append(button);
 	}
+};
+
+// click button to call gify api and show images
+$(document).on("click", "button", function() {
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + selection;
+	selection = $(this).attr("data-id");
+	console.log(selection);
+	 $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+          var results = response.data;
+          console.log(results);
+          for (var i = 0; i < results.length; i++) {
+          	var animalsDiv = $("<div>");
+          	var p = $("<p>").text("Rating: " + results[i].rating);
+          	var image = $("<img>");
+          	image.attr("src", results[i].images.fixed_height.url);
+          	animalsDiv.append(p);
+          	animalsDiv.append(image);
+          	console.log(animalsDiv);
+          	$("#animals").prepend(animalsDiv);
+          }
+      });
+
+
 });
+
+$(document).ready(function() {
+	showButtons();
+	});
 
 
