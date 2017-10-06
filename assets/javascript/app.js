51aaecc6eb952a7c5cb7a2220924ccd94d0b54fe
Matthew Nagy <mattn@matts-mbp.attlocal.net>
2017-10-06 08:43:00 -0400

    set animate attributes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,16 @@ var selection = 0;
 
 $(document).ready(function() {
 // display buttons from array
-function showButtons() {
-	$("#animalButtons").empty();
-	for(i = 0; i < animalArray.length; i++) {
-		var choice = animalArray[i];
-		var button = $('<button>');
-		button.text(choice);
-		button.attr('data-id', choice);
-		$('#animalButtons').append(button);
-	}
-};
+	function showButtons() {
+		$("#animalButtons").empty();
+		for(i = 0; i < animalArray.length; i++) {
+			var choice = animalArray[i];
+			var button = $('<button>');
+			button.text(choice);
+			button.attr('data-id', choice);
+			$('#animalButtons').append(button);
+		}
+	};
 
 // click button to call gify api and append images to div
 	$("#animalButtons").on("click","button",function() {
@@ -25,16 +25,36 @@ function showButtons() {
 	        })
 	        .done(function(response) {
 	          var results = response.data;
+	          console.log(results);
 	          for (var i = 0; i < results.length; i++) {
+	          	var animalAnimate = results[i].images.fixed_height.url;
+	          	var animalStill = results[i].images["480w_still"].url;
 	          	var animalsDiv = $("<div class='animalsDiv'>");
 	          	var p = $("<p>").text("Rating: " + results[i].rating);
 	          	var image = $("<img>");
-	          	image.attr("src", results[i].images.fixed_height.url);
+	          	image.attr("class", "animalIcon")
+	          	.attr("data-still", animalStill)
+	          	.attr("data-Animate", animalAnimate)
+	          	.attr("src", animalStill)
 	          	animalsDiv.append(p);
 	          	animalsDiv.append(image);
 	          	$("#animals").append(animalsDiv);
 	          }
 	      });
+	});
+
+	$("img").on("click",function() {
+		alert("click")
+;		var state = $(this).attr("data-state");
+		if (state === "still") {
+			alert("test");
+			$(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+		} else {
+			alert("test2");
+			$(this).attr("src", $(this).attr("data-still"));
+	        $(this).attr("data-state", "still");
+		}
 	});
 
 // add a new animal to the array
